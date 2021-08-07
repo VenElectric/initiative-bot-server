947797fe96826f0454492615e780a6e3c9fa1033
Andrew <andrew.kent.90@gmail.com>
2021-08-07 13:55:18 -0700

    adding process6

--- processes/firebasesetup.js
+++ processes/firebasesetup.js
@@ -2,8 +2,6 @@ const admin = require("firebase-admin");
 const process = require('process');
 // let serviceAccount = require("../firebase.json");
 
-let serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii'))
-
 admin.initializeApp({
   credential: admin.credential.cert({
       projectId: "dungeon-bot-4e0e8", 
