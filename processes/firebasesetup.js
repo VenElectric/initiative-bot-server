d0d1184adc31344803e8d14d1a396d1246bf4553
Andrew <andrew.kent.90@gmail.com>
2021-08-07 13:41:16 -0700

    adding process5

--- processes/firebasesetup.js
+++ processes/firebasesetup.js
@@ -2,10 +2,10 @@ const admin = require("firebase-admin");
 const process = require('process');
 // let serviceAccount = require("../firebase.json");
 
-let serviceAccount = JSON.parse(Buffer.from(String(process.env.GOOGLE_CONFIG), 'base64').toString('ascii'))
+let serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii'))
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(Buffer.from(String(process.env.GOOGLE_CONFIG), 'base64').toString('ascii')))
+  credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii')))
 })
 
 const db = admin.firestore()
