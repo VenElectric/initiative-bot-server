d268426c5598902dd5e25a5de7e1e4bd3aa158fa
Andrew <andrew.kent.90@gmail.com>
2021-08-07 13:05:48 -0700

    more changes

---
+++ processes/firebasesetup.js
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin");
+// let serviceAccount = require("../firebase.json");
+let serviceAccount = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii'))
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
+module.exports = {db}
+  
