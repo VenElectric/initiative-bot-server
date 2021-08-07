03e76b60ef9e914bff2cbf8faf85c596ff6208c8
Andrew <andrew.kent.90@gmail.com>
2021-08-07 16:00:16 -0700

    try again

--- processes/firebasesetup.js
+++ processes/firebasesetup.js
@@ -2,11 +2,14 @@ const admin = require("firebase-admin");
 const process = require('process');
 // let serviceAccount = require("../firebase.json");
 
+const googleServiceAccountCreds = process.env.GOOGLE_CONFIG;
+if (!googleServiceAccountCreds) throw new Error('The $GOOGLE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
+
 admin.initializeApp({
   credential: admin.credential.cert({
       projectId: "dungeon-bot-4e0e8", 
       clientEmail: process.env.clientemail, 
-      privateKey: process.env.GOOGLE_CONFIG.replace(/\\n/g, '\n')
+      privateKey: googleServiceAccountCreds
   }),
 })
 
