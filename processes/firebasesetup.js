dc966e98ee237e532be68d8400e3081624660de0
Andrew <andrew.kent.90@gmail.com>
2021-08-07 19:49:46 -0700

    make it better

--- processes/firebasesetup.js
+++ processes/firebasesetup.js
@@ -2,11 +2,11 @@ const admin = require("firebase-admin");
 
 // let serviceAccount = require("../firebase.json");
 
-const googleServiceAccountCreds = Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii')
+const googleServiceAccountCreds = JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii'))
 if (!googleServiceAccountCreds) throw new Error('The $GOOGLE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG, 'base64').toString('ascii'))),
+  credential: admin.credential.cert(googleServiceAccountCreds),
 })
 
 const db = admin.firestore()
