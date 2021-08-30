27c99ed8ec8752e9c8547b75862c3b816c102567
Andrew <andrew.kent.90@gmail.com>
2021-08-30 13:17:47 -0700

    notes

--- logging/logger.js
+++ logging/logger.js
@@ -5,7 +5,9 @@ const { combine, timestamp, label, printf } = format;
 const myFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
- 
+
+// need to remove logger
+
 const logger = createLogger({
   level: 'info',
   format: myFormat,
